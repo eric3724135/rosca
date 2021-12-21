4f39b5569fd43c75070cf7e1b33f74d8f5890110
eric <eric3721435@gmail.com>
2021-12-22 00:31:22 +0800

    member and association

---
+++ src/main/java/com/eric/rosca/domain/Association.java
@@ -0,0 +1,19 @@
+package com.eric.rosca.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 互助會每個會期
+ */
+public class Association {
+
+    public static final int CHARGE = 9000;
+
+    public static final int MANEGEMENT_FEE = 200;
+
+    private List<Member> members = new ArrayList<>();
+
+
+
+}
--- src/main/java/com/eric/rosca/domain/Member.java
+++ src/main/java/com/eric/rosca/domain/Member.java
@@ -1,4 +1,22 @@
 package com.eric.rosca.domain;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 會員
+ */
 public class Member {
+
+    private int id;
+
+    private String name;
+
+    /** 預設資金 600w*/
+    private BigDecimal funds = new BigDecimal(6000000);
+
+    private Map<String, List<Association>> joinedAssoiations = new HashMap<>();
+
 }
